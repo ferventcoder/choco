6e83c7b92df8b1cec8a8dd9009c2f0d636acf3f8
Rob Reynolds <ferventcoder@gmail.com>
2015-01-22 00:27:53 -0600

    (GH-14) add source runner to applicationparameters

--- src/chocolatey/infrastructure.app/ApplicationParameters.cs
+++ src/chocolatey/infrastructure.app/ApplicationParameters.cs
@@ -51,6 +51,7 @@ namespace chocolatey.infrastructure.app
         public static string ShimsLocation = _fileSystem.combine_paths(InstallLocation, "bin");
         public static string ChocolateyPackageInfoStoreLocation = _fileSystem.combine_paths(InstallLocation, ".chocolatey");
         public static readonly string ChocolateyCommunityFeedPushSource = "https://chocolatey.org/";
+        public static readonly string ChocolateyCommunityFeedSource = "https://chocolatey.org/api/v2/";
         public static readonly string UserAgent = "Chocolatey Command Line";
         public static readonly string RegistryValueInstallLocation = "InstallLocation";
         public static readonly string RollbackPackageSuffix = "._.previous";
@@ -77,6 +78,11 @@ namespace chocolatey.infrastructure.app
         {
             public static readonly string ContinueChocolateyAction = "Moving forward with chocolatey actions.";
             public static readonly string NugetEventActionHeader = "Nuget called an event";
+        }
+
+        public static class SourceRunner
+        {
+            public static readonly string WebPiPackage = "webpicmd";
         }
 
         public static class OutputParser
